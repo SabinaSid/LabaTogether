c2ab7c0a291ac3a63ee83b50214131aeb87bf132
AnnaDenisovaIst17 <denisovaa.a@mail.ru>
2020-03-18 20:07:54 +0300

    Add new views

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ module.exports = (r, q) => {
     db.getTasks().then(task =>{
         db.getStatuses().then (statuses => {
             let model = new MainViewModel('TODO LIST',task, statuses);
-            q.render('index', model);
+            q.render('indexMain', model);
         });
     });
 }
