aa97c053127a2a346d5bf6b08bce7c62943e0d00
Sabinca <kaiser2169@gmail.com>
2020-03-21 11:57:38 +0300

    Создана бд. Ее круды. Удалены ненужные файлы.

--- utils/sqlitedb.js
+++ utils/sqlitedb.js
@@ -1,26 +1,46 @@
 const sqlite = require('sqlite3').verbose();
-const path = 'todo.db';
+const path = 'VetClinic.db';
 
-function connection(){
+function connection() {
     return new sqlite.Database(path, err => {
         if (err) console.error('Database connect. ', err.message);
         else console.log('Database is connected');
     });
 }
 
-exports.getStatuses = function(id) {
+exports.getTypeAnimal = function (id) {
     return new Promise((resolve, reject) => {
-        let query = 'Select * FROM Status';
+        let query = 'Select * FROM TypeAnimal';
         let params = [];
         if (id) {
             query += ' WHERE ID = ?';
             params.push(id);
         }
 
-        connection().all(query, params, (err,result) => {
-            if(err) reject(err);
-            else{
-                if(id){
+        connection().all(query, params, (err, result) => {
+            if (err) reject(err);
+            else {
+                if (id) {
+                    result = result[0];
+                }
+                resolve(result);
+            }
+        }).close();
+    });
+}
+exports.getService = function (id) {
+    return new Promise((resolve, reject) => {
+        let query = 'Select * FROM Service';
+        let params = [];
+        if (id) {
+            query += ' WHERE ID = ?';
+            params.push(id);
+        }
+
+        connection().all(query, params, (err, result) => {
+            if (err) reject(err);
+            else {
+                if (id) {
                     result = result[0];
                 }
                 resolve(result);
@@ -29,145 +49,130 @@ exports.getStatuses = function(id) {
     });
 }
 
-exports.getTasks = function(id) {
+exports.getRecords = function (id) {
     return new Promise((resolve, reject) => {
         let context = connection();
-        let query = 'SELECT * FROM Status';
+        let query = `select 
+        r.id,
+        r.Date,
+        r.Time,
+        r.NameOwner,
+        r.NumberOwner,
+        r.NameAnimal,
+        t.Name,
+        s.Name,
+        s.Price
+        FROM Record r 
+        join Service s on r.IDService=s.ID
+        join TypeAnimal t on r.IDTypeAnimal=t.ID`;
 
-        context.all(query, (err, statuses) => {
-            if(err) {
+        if (id) {
+            query += ' WHERE ID = ?';
+            params.push(id);
+        }
+
+        context.all(query, (err, result) => {
+            if (err) 
                 reject(err);
-            }
-            else{
-                query = 'SELECT * FROM Task';
-                let params = [];
-                if(id) {
-                    query += ' WHERE ID = ?';
-                    params.push(id);
-                }
+            
+            else {
+                    if (id) {
+                        result = result[0];
+                    }
+                    resolve(result);
 
-                context.all(query, params, (err, result) => {
-                    if(err){
-                        reject(err);
-                    }
-                    else {
-                        result.forEach(x=>{
-                            x.status = statuses.filter(y => y.id === x.idStatus)[0];
-                            delete x.idStatus;
-                        });
-                        if(id){
-                            result = result[0];
-                        }
-                        resolve(result);
-                    }
-                });
-            }
+                };
+            
         }).close();
     });
 }
 
-exports.addTask = function(task) {
-    return new Promise((resolve,reject) => {
-        if(!task){
-            reject({message: 'task is empty'});
+exports.addRecord = function (record) {
+    return new Promise((resolve, reject) => {
+        if (!task) {
+            reject({ message: 'task is empty' });
             return;
         }
         let dateNow = new Date().toISOString().split('T')[0];
-        let query = 'INSERT INTO Task (title, startDate, endDate, idStatus) VALUES(?,?,?,?)';
+        let query = 'INSERT INTO Record (Date, Time, NameOwner, NumberOwner,NameAnimal,IDTypeAnimal,IDService) VALUES(?,?,?,?,?,?,?)';
         let params = [
-            task.title || '',
-            task.startDate || dateNow,
-            task.endDate || dateNow,
-            task.status.id || null,
+            record.Date || dateNow,
+            record.Time || '',
+            record.NameOwner || '',
+            record.NumberOwner || '',
+            record.NameAnimal || '',
+            record.IDTypeAnimal || 1,
+            record.IDService || 1,
         ];
-        connection().run(query,params,(err, result) => {
-            if(err) reject(err);
+        connection().run(query, params, (err, result) => {
+            if (err) reject(err);
             else resolve(result);
         }).close();
     });
 }
 
-exports.updateTask = function(task) {
-    return new Promise((resolve,reject) => {
-        if(!task.id){
-            reject({message: 'task is empty'});
+exports.updateTask = function (record) {
+    return new Promise((resolve, reject) => {
+        if (!record.id) {
+            reject({ message: 'record is empty' });
             return;
         }
         let dateNow = new Date().toISOString().split('T')[0];
-        let query = `UPDATE Task SET title = ? 
-                                     , startDate = ? 
-                                     , endDate = ? 
-                                     , idStatus = ?
+        let query = `UPDATE Record SET 
+                                        Date = ?,
+                                        Time = ?,
+                                        NameOwner = ?,
+                                        NumberOwner = ?,
+                                        NameAnimal = ?,
+                                        IDTypeAnimal =?,
+                                        IDService =?,
                                       WHERE id = ?`;
         let params = [
-            task.title || '',
-            task.startDate || dateNow,
-            task.endDate || dateNow,
-            task.status.id || null,
-            task.id,
+            record.Date || dateNow,
+            record.Time || '',
+            record.NameOwner || '',
+            record.NumberOwner || '',
+            record.NameAnimal || '',
+            record.IDTypeAnimal || 1,
+            record.IDService || 1,
+            record.id
         ];
         console.log(params);
-        connection().run(query,params, (err,result) => {
-            if(err) reject(err);
+        connection().run(query, params, (err, result) => {
+            if (err) reject(err);
             else resolve(result);
         }).close();
     });
 }
 
-exports.removeTask = function(id) {
-    return new Promise((resolve,reject)=>{
-        if(!id){
-            reject({message: 'id is empty'});
+exports.removeTask = function (id) {
+    return new Promise((resolve, reject) => {
+        if (!id) {
+            reject({ message: 'id is empty' });
             return;
         }
 
-        let query = "DELETE FROM Task WHERE ID = ?";
+        let query = "DELETE FROM Record WHERE ID = ?";
         let params = [id];
 
-        connection().run(query,params, (err, result)=>{
-            if(err) reject(err);
+        connection().run(query, params, (err, result) => {
+            if (err) reject(err);
             else resolve(result);
         }).close();
     });
 }
 
-exports.getLastTask = function(){
-    return new Promise((resolve, reject)=>{
+exports.getLastTask = function () {
+    return new Promise((resolve, reject) => {
         let context = connection();
-        let query = `SELECT * FROM Task ORDER BY id DESC LIMIT 1`;
+        let query = `SELECT * FROM Record ORDER BY id DESC LIMIT 1`;
 
-        context.all(query,(err, result)=>{
-            if(err){
+        context.all(query, (err, result) => {
+            if (err) {
                 reject(err);
             }
             else {
                 result = result ? result[0] : null;
-                resolve(result);
-            }
-        }).close();
-    });
-}
-
-exports.getRole = function ({login, password}){
-    return new Promise((resolve, reject)=>{
-        let context = connection();
-        let query = `SELECT TypeUser.id FROM User
-                     INNER JOIN TypeUser ON TypeUser.id = User.idTypeUser
-                     WHERE login = ? AND password = ? LIMIT 1`;
-        let params = [ login, password];
-
-        context.all(query, params, (err, result)=>{
-            if(err){
-                reject(err);
-            }
-            else {
-                if(result && result[0]){
-                    result = result[0].id == 1
-                        ?'User'
-                        : (result[0].id == 2 ? 'Admin' : null);
-                } else {
-                    result = null;
-                }
                 resolve(result);
             }
         }).close();
